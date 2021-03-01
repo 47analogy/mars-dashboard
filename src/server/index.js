9b65db268056265fcd97380b74e6ec5572428c9b
analogy47 <actweed47@gmail.com>
2021-02-28 19:49:55 -0500

    server: fetch-data-for-three-rovers

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,7 @@
 require('dotenv').config();
 const express = require('express');
-//const bodyParser = require('body-parser')
 const fetch = require('node-fetch');
 const path = require('path');
-
 const app = express();
 const port = 3000;
 
@@ -12,31 +10,67 @@ app.use(express.json());
 
 app.use('/', express.static(path.join(__dirname, '../public')));
 
-// your API calls
+// Curiosity Rover
+app.get('/curiosity', async (req, res) => {
+  let mostRecentSol;
 
-// example API call
-app.get('/apod', async (req, res) => {
   try {
-    let image = await fetch(
-      `https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`
+    const manifest = await fetch(
+      `https://api.nasa.gov/mars-photos/api/v1/manifests/curiosity/?api_key=${process.env.API_KEY}`
     ).then((res) => res.json());
-    console.log(image);
-    res.send({ image });
+    mostRecentSol = manifest.photo_manifest.max_sol;
+    console.log('max_sol', manifest.photo_manifest.max_sol);
+
+    // use sol to get images
+    const { photos } = await fetch(
+      `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${mostRecentSol}&api_key=${process.env.API_KEY}`
+    ).then((res) => res.json());
+    res.send(photos);
+  } catch (err) {
+    console.log('error:', err);
+  }
+});
+
+// Opportunity rover
+app.get('/opportunity', async (req, res) => {
+  let mostRecentSol;
+
+  try {
+    const manifest = await fetch(
+      `https://api.nasa.gov/mars-photos/api/v1/manifests/opportunity/?api_key=${process.env.API_KEY}`
+    ).then((res) => res.json());
+
+    mostRecentSol = manifest.photo_manifest.max_sol;
+
+    const { photos } = await fetch(
+      `https://api.nasa.gov/mars-photos/api/v1/rovers/opportunity/photos?sol=${mostRecentSol}&api_key=${process.env.API_KEY}`
+    ).then((res) => res.json());
+
+    res.send(photos);
+  } catch (err) {
+    console.log('error:', err);
+  }
+});
+
+// Spirit rover
+app.get('/spirit', async (req, res) => {
+  let mostRecentSol;
+
+  try {
+    const manifest = await fetch(
+      `https://api.nasa.gov/mars-photos/api/v1/manifests/spirit/?api_key=${process.env.API_KEY}`
+    ).then((res) => res.json());
+
+    mostRecentSol = manifest.photo_manifest.max_sol;
+
+    const { photos } = await fetch(
+      `https://api.nasa.gov/mars-photos/api/v1/rovers/spirit/photos?sol=${mostRecentSol}&api_key=${process.env.API_KEY}`
+    ).then((res) => res.json());
+
+    res.send(photos);
   } catch (err) {
     console.log('error:', err);
   }
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
-/*
-{
-  date: '2021-01-17',
-  explanation: 'The jets emanating from Centaurus A are over a million light years long. These jets of streaming plasma, expelled by a giant black hole in the center of this spiral galaxy,  light up this composite image of Cen A. Exactly how the central black hole expels infalling matter remains unknown. After clearing the galaxy, however, the jets inflate large radio bubbles that likely glow for millions of years. If energized by a passing gas cloud, the radio bubbles can even light up again after billions of years. X-ray light is depicted in the featured composite image in blue, while microwave light is colored orange.  The base of the jet in radio light shows details of the innermost light year of the central jet.',
-  hdurl: 'https://apod.nasa.gov/apod/image/2101/CenAjets_EsoNasa_1280.jpg',
-  media_type: 'image',
-  service_version: 'v1',
-  title: 'Jets from Unusual Galaxy Centaurus A',
-  url: 'https://apod.nasa.gov/apod/image/2101/CenAjets_EsoNasa_960.jpg'
-}
-*/
