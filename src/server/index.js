67e744035bf2d1ddbcf396dd385901e3bdf9f961
analogy47 <actweed47@gmail.com>
2021-03-04 23:55:44 -0500

    backend-refactor

--- src/server/index.js
+++ src/server/index.js
@@ -10,64 +10,13 @@ app.use(express.json());
 
 app.use('/', express.static(path.join(__dirname, '../public')));
 
-// Curiosity Rover
-app.get('/curiosity', async (req, res) => {
-  let mostRecentSol;
-
+app.get(`/rover/:roverName`, async (req, res) => {
+  const roverName = req.params.roverName;
   try {
-    const manifest = await fetch(
-      `https://api.nasa.gov/mars-photos/api/v1/manifests/curiosity/?api_key=${process.env.API_KEY}`
+    const { latest_photos } = await fetch(
+      `https://api.nasa.gov/mars-photos/api/v1/rovers/${roverName}/latest_photos?api_key=${process.env.API_KEY}`
     ).then((res) => res.json());
-    mostRecentSol = manifest.photo_manifest.max_sol;
-    console.log('max_sol', manifest.photo_manifest.max_sol);
-
-    // use sol to get images
-    const { photos } = await fetch(
-      `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${mostRecentSol}&api_key=${process.env.API_KEY}`
-    ).then((res) => res.json());
-    res.send(photos);
-  } catch (err) {
-    console.log('error:', err);
-  }
-});
-
-// Opportunity rover
-app.get('/opportunity', async (req, res) => {
-  let mostRecentSol;
-
-  try {
-    const manifest = await fetch(
-      `https://api.nasa.gov/mars-photos/api/v1/manifests/opportunity/?api_key=${process.env.API_KEY}`
-    ).then((res) => res.json());
-
-    mostRecentSol = manifest.photo_manifest.max_sol;
-
-    const { photos } = await fetch(
-      `https://api.nasa.gov/mars-photos/api/v1/rovers/opportunity/photos?sol=${mostRecentSol}&api_key=${process.env.API_KEY}`
-    ).then((res) => res.json());
-
-    res.send(photos);
-  } catch (err) {
-    console.log('error:', err);
-  }
-});
-
-// Spirit rover
-app.get('/spirit', async (req, res) => {
-  let mostRecentSol;
-
-  try {
-    const manifest = await fetch(
-      `https://api.nasa.gov/mars-photos/api/v1/manifests/spirit/?api_key=${process.env.API_KEY}`
-    ).then((res) => res.json());
-
-    mostRecentSol = manifest.photo_manifest.max_sol;
-
-    const { photos } = await fetch(
-      `https://api.nasa.gov/mars-photos/api/v1/rovers/spirit/photos?sol=${mostRecentSol}&api_key=${process.env.API_KEY}`
-    ).then((res) => res.json());
-
-    res.send(photos);
+    res.json(latest_photos);
   } catch (err) {
     console.log('error:', err);
   }
